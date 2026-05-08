176d821e09aa5d858896d325adf159286783b220
clydeflow <sybdr6rcpd@privaterelay.appleid.com>
2026-05-08 19:51:11 +0800

    feat: add App.vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <DevicePreview><RouterView /></DevicePreview>
+</template>
+<script setup lang="ts">
+import DevicePreview from './components/DevicePreview.vue'
+</script>
+<style>*{margin:0;padding:0;box-sizing:border-box}body{font-family:-apple-system,BlinkMacSystemFont,'PingFang SC',sans-serif}</style>
